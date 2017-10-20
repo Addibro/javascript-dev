05c73d325d1e938a7f3f553606c169c7794bb71a
andreas <andreas.alme.eriksson@gmail.com>
2017-10-20 00:39:56 -0400

    add babelrx, srcSercer.js startMessage.js and json

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
-var express = require('express'); // get express
-var path = require('path'); // reference to path
-var open = require('open'); // reference to open, which we'll use our site in the browser
+// var express = require('express'); // get express
+// var path = require('path'); // reference to path
+// var open = require('open'); // reference to open, which we'll use our site in the browser
+// var port = 3000; // port to use
+// var app = express(); // create an instance of express
 
-var port = 3000; // port to use
-var app = express(); // create an instance of express
+// Thanks to babel we can use ES6 syntax instead of node's (above)
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+const app = express();
 
 
 app.get('/', function (req, res) {  // "any references to the root should be handled by this function, which takes and request and a response"
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,7 @@
+// chalk is a package that allows you to specify the color of the output to console
+// var chalk = require('chalk');
+
+// since we use babel we can use the ES6 syntax like so:
+import chalk from 'chalk';
+
+console.log(chalk.green("Starting app in dev mode..."));
